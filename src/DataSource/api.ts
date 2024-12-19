c9389f483cfad286b823b5305054644368f5e184
Alexandre <alexandre.delesse@isen.yncrea.fr>
2024-12-19 21:22:09 +0100

    change to 8090

--- src/DataSource/api.ts
+++ src/DataSource/api.ts
@@ -9,7 +9,7 @@ import { Signature } from "../Domain/Signature";
 import { WebApi } from "./WebApi/WebApi";
 
 const api = axios.create({
-  baseURL: "http://intranet.urgencesante.fr:8085/api",
+  baseURL: "http://intranet.urgencesante.fr:8090/api",
   timeout: 1000,
 });
 
