3b1dcb9dfd012b416caee761b34acdf0d2444c49
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2024-12-19 21:37:12 +0100

    fix error on redirect to login

--- src/Router/PrivateRoute.tsx
+++ src/Router/PrivateRoute.tsx
@@ -1,25 +1,25 @@
-import { Box } from '@mui/system';
+import { Box } from '@mui/system'
 import React, { useEffect } from 'react'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
-import { getCrew } from '../DataSource/localStorage';
-import MainNavbarFacade from '../Presenter/components/MainNavbar/MainNavbarFacade';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { getCrew } from '../DataSource/localStorage'
+import MainNavbarFacade from '../Presenter/components/MainNavbar/MainNavbarFacade'
 
 export default function PrivateRoute() {
-    const navigate = useNavigate();
-    const location = useLocation();
-  
-    useEffect(() => {
-      if (location.pathname === "/regul") return;
-      const crew = getCrew();
-      if (!crew) navigate("login");
-    }, [navigate]);
-  
-    return (
-      <>
-        <MainNavbarFacade />
-        <Box sx={{ padding: "16px" }}>
-          <Outlet />
-        </Box>
-      </>
-    );
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname === '/regul') return
+    const crew = getCrew()
+    if (!crew) navigate('/login', { replace: true })
+  }, [navigate])
+
+  return (
+    <>
+      <MainNavbarFacade />
+      <Box sx={{ padding: '16px' }}>
+        <Outlet />
+      </Box>
+    </>
+  )
 }
