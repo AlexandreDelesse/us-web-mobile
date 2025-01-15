5f958f176627d5aa5f11046d074a4dd2a1e51356
Alexandre <alexandre.delesse@isen.yncrea.fr>
2025-01-15 23:45:44 +0100

    remove the add of Z on isodate

--- src/Presenter/page/JobDetailNavigation/JobDetail/StepProgress/StepProgressDecorator.tsx
+++ src/Presenter/page/JobDetailNavigation/JobDetail/StepProgress/StepProgressDecorator.tsx
@@ -14,8 +14,9 @@ export default function StepProgressDecorator({
   const setZoulouToIsoDate = (el: string) =>
     el.charAt(el.length - 1) !== "Z" ? el + "Z" : el;
 
+  console.log("go", go, "onSite", onSite, "available", available);
   const formattedInitialSteps = {
-    go: go ? new Date(setZoulouToIsoDate(go)) : null,
+    go: go ? new Date(go) : null,
     onSite: onSite ? new Date(setZoulouToIsoDate(onSite)) : null,
     available: available ? new Date(setZoulouToIsoDate(available)) : null,
   };
